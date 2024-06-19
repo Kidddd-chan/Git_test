779345091df5e35de9c0b38c00143bb4c8cc553e
Kidddd-chan <teamgamozxv@gmail.com>
2024-06-19 23:20:26 +0700

    add file Password

---
+++ Xedap.py
@@ -0,0 +1,9 @@
+class XeDap:
+    def __init__(self):
+        pass
+
+
+xeDap1 = XeDap()
+xeDap1.name = 'Xe đạp điện'
+xeDap2 = XeDap()
+xeDap2.name = 'Xe đạp đua'
---
+++ password_random.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+Letters = string.ascii_letters
+Numbers = string.digits
+Punctuation = string.punctuation
+
